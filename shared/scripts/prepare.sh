cecf2a60d19bb8eeeddddb84fb50c66aa82ae1a5
Petr Kulhanek <kulhanek@chemi.muni.cz>
2026-09-21 10:38:19 +0200

    ensure all files are copied

--- shared/scripts/prepare.sh
+++ shared/scripts/prepare.sh
@@ -9,5 +9,5 @@ if ! [[ -f ../shared/_TEACHER_ONLY_ ]]; then
     echo ">>> ERROR: This script must be run in a student's week directory!"
     exit 1
 fi
-cp -r ../shared/materials/$WEEK/* ./
+cp -r ../shared/materials/$WEEK/. ./
 
